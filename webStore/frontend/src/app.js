93d3dab1372885a58d5a1b65bcc8f201227b84db
mminic17 <minicmilos17@gmail.com>
2021-07-06 21:42:20 +0200

    Styling HomeScreen - List of Products

--- webStore/frontend/src/app.js
+++ webStore/frontend/src/app.js
@@ -2,10 +2,8 @@ import HomeScreen from "./screens/HomeScreen.js";
 
 const router = async () => {
   const main = document.getElementById("main-container");
-  //main.innerHTML = await HomeScreen.render();
+  main.innerHTML = await HomeScreen.render();
 };
-
-//window.addEventListener("load", router);
 
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
@@ -14,3 +12,5 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+window.addEventListener("load", router);
--- webStore/frontend/src/screens/HomeScreen.js
+++ webStore/frontend/src/screens/HomeScreen.js
@@ -10,23 +10,37 @@ const HomeScreen = {
     //const { products } = data; -> ES6 property products from data.js
     let products = await getProducts();
     return `
+    <div class="button_grp">
+          <ul>
+            <li data-li="all" class="button active">All</li>
+            <li data-li="t-Shirts" class="button">T-Shirts</li>
+            <li data-li="tracksuits" class="button">Tracksuits</li>
+            <li data-li="slippers" class="button">Slippers</li>
+            <li data-li="sweatshirts" class="button">Sweatshirts</li>
+            <li data-li="waistcoats" class="button">Waistcoats</li>
+            <li data-li="watches" class="button">Watches</li> 
+          </ul>
+    </div>
+        <br><br>
+        </div>
         <ul class="products">
         ${products
           .map(
             (product) => `
         <li>
           <div class="product">
-            <a href="/#/product/${product._id}">
+          <a href="/#/product/${product._id}">
                 <img src="${product.image}" alt="${product.name}" />
             </a>
             <div class="product-name">
-                <a href="/#/product/${product._id}"> ${product.name} </a>
+                <a href="/#/product/${product._id}"><h1> ${product.name} </h1></a>
             </div>
+            <hr />
             <div class="product-brand">
-                ${product.brand}
+               <h2> ${product.brand} </h2>
             </div>
             <div class="product-price">
-                $${product.price}
+               <h1> $${product.price} </h1>
             </div>
           </div>
         </li>
